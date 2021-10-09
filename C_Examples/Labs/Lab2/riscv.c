22b28a758dac7a3bce3700563673248592035afc
SebastianServin <servinseb@hotmail.com>
2021-10-09 12:15:42 -0600

    Finished logic of lab2, comments are missing

--- C_Examples/Labs/Lab2/riscv.c
+++ C_Examples/Labs/Lab2/riscv.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-// #include <string.h>
 #include <stdlib.h>	   // malloc & free
 #include <stdint.h>	   // use guaranteed 64-bit integers
 #include "tokenizer.h" // Create header file and reference that
@@ -62,7 +61,7 @@ int customSwitch(char *str)
 
 void addi(char **tokens)
 {
-	int store_in = -1;
+	int32_t store_in = -1;
 	char *to_store_in_pointer = *tokens;
 	*to_store_in_pointer++;
 	for (; *to_store_in_pointer; *to_store_in_pointer++)
@@ -79,7 +78,7 @@ void addi(char **tokens)
 	}
 
 	**tokens++;
-	int add_one = -1;
+	int32_t add_one = -1;
 	char *add_one_pointer = *tokens;
 	*add_one_pointer++;
 	for (; *add_one_pointer; *add_one_pointer++)
@@ -97,7 +96,7 @@ void addi(char **tokens)
 
 	**tokens++;
 	bool negative = false;
-	int add_two = -1;
+	int32_t add_two = -1;
 	char *add_two_pointer = *tokens;
 	if (*add_two_pointer == '-')
 	{
@@ -106,6 +105,10 @@ void addi(char **tokens)
 	}
 	for (; *add_two_pointer; *add_two_pointer++)
 	{
+		if (*add_two_pointer == '\n' || *add_two_pointer == '\0')
+		{
+			break;
+		}
 		if (add_two == -1)
 		{
 			add_two = *add_two_pointer - '0';
@@ -126,7 +129,7 @@ void addi(char **tokens)
 
 void add(char **tokens)
 {
-	int store_in = -1;
+	int32_t store_in = -1;
 	char *to_store_in_pointer = *tokens;
 	*to_store_in_pointer++;
 	for (; *to_store_in_pointer; *to_store_in_pointer++)
@@ -143,7 +146,7 @@ void add(char **tokens)
 	}
 
 	**tokens++;
-	int add_one = -1;
+	int32_t add_one = -1;
 	char *add_one_pointer = *tokens;
 	*add_one_pointer++;
 	for (; *add_one_pointer; *add_one_pointer++)
@@ -160,12 +163,16 @@ void add(char **tokens)
 	}
 
 	**tokens++;
-	int add_two = -1;
+	int32_t add_two = -1;
 	char *add_two_pointer = *tokens;
 	*add_two_pointer++;
 
 	for (; *add_two_pointer; *add_two_pointer++)
 	{
+		if (*add_two_pointer == '\n' || *add_two_pointer == '\0')
+		{
+			break;
+		}
 		if (add_two == -1)
 		{
 			add_two = *add_two_pointer - '0';
@@ -180,12 +187,158 @@ void add(char **tokens)
 	reg[store_in] = reg[add_one] + reg[add_two];
 }
 
-void sw(char **tokens) {
+void sw(char **tokens)
+{
+	char *mem_file = "mem.txt";
+	int32_t get_from = -1;
+	char *get_from_pointer = *tokens;
+	*get_from_pointer++;
+	for (; *get_from_pointer; *get_from_pointer++)
+	{
+		if (get_from == -1)
+		{
+			get_from = *get_from_pointer - '0';
+		}
+		else
+		{
+			get_from = get_from * 10;
+			get_from = get_from + (*get_from_pointer - '0');
+		}
+	}
+	**tokens++;
 
+	bool offset_initialized = false;
+	int32_t offset;
+	char *offset_pointer = *tokens;
+
+	for (; *offset_pointer; *offset_pointer++)
+	{
+		if (*offset_pointer == '(')
+		{
+			break;
+		}
+		if (!offset_initialized)
+		{
+			offset_initialized = true;
+			offset = *offset_pointer - '0';
+		}
+		else
+		{
+			offset = offset * 10;
+			offset = offset + (*offset_pointer - '0');
+		}
+	}
+
+	*offset_pointer++;
+
+	bool memory_address_initialized = false;
+	int32_t memory_address;
+
+	for (; *offset_pointer; *offset_pointer++)
+	{
+		if (*offset_pointer == ')')
+		{
+			break;
+		}
+		if (!memory_address_initialized)
+		{
+			memory_address_initialized = true;
+			memory_address = *offset_pointer - '0';
+		}
+		else
+		{
+			memory_address = memory_address * 10;
+			memory_address = memory_address + (*offset_pointer - '0');
+		}
+	}
+
+	int32_t store_in = offset + memory_address;
+
+	int32_t data_to_write = reg[get_from];
+
+	int32_t write = write_address(data_to_write, store_in, mem_file);
+	if (write == (int32_t)NULL)
+		printf("ERROR: Unsucessful write to address %0X\n", 0x40);
+	// int32_t read = read_address(get_from, mem_file);
+
+	// printf("GET FROM DECIMAL: %i\n", get_from);
+	// printf("GET FROM HEX: %lX\n", get_from);
+	// printf("Read address %lu (0x%lX): %lu (0x%lX)\n", get_from, get_from, read, read); // %lu -> format as an long-unsigned
+	// reg[store_in] = read;
 }
 
-void lw(char **tokens) {
-	
+void lw(char **tokens)
+{
+	char *mem_file = "mem.txt";
+	int32_t store_in = -1;
+	char *to_store_in_pointer = *tokens;
+	*to_store_in_pointer++;
+	for (; *to_store_in_pointer; *to_store_in_pointer++)
+	{
+		if (store_in == -1)
+		{
+			store_in = *to_store_in_pointer - '0';
+		}
+		else
+		{
+			store_in = store_in * 10;
+			store_in = store_in + (*to_store_in_pointer - '0');
+		}
+	}
+	**tokens++;
+
+	bool offset_initialized = false;
+	int32_t offset;
+	char *offset_pointer = *tokens;
+
+	for (; *offset_pointer; *offset_pointer++)
+	{
+		if (*offset_pointer == '(')
+		{
+			break;
+		}
+		if (!offset_initialized)
+		{
+			offset_initialized = true;
+			offset = *offset_pointer - '0';
+		}
+		else
+		{
+			offset = offset * 10;
+			offset = offset + (*offset_pointer - '0');
+		}
+	}
+
+	*offset_pointer++;
+
+	bool memory_address_initializd = false;
+	int32_t memory_address;
+
+	for (; *offset_pointer; *offset_pointer++)
+	{
+		if (*offset_pointer == ')')
+		{
+			break;
+		}
+		if (!memory_address_initializd)
+		{
+			memory_address_initializd = true;
+			memory_address = *offset_pointer - '0';
+		}
+		else
+		{
+			memory_address = memory_address * 10;
+			memory_address = memory_address + (*offset_pointer - '0');
+		}
+	}
+
+	int32_t get_from = offset + memory_address;
+	int32_t read = read_address(get_from, mem_file);
+
+	// printf("GET FROM DECIMAL: %i\n", get_from);
+	// printf("GET FROM HEX: %lX\n", get_from);
+	printf("Read address %lu (0x%lX): %lu (0x%lX)\n", get_from, get_from, read, read); // %lu -> format as an long-unsigned
+	reg[store_in] = read;
 }
 /**
  * Fill out this function and use it to read interpret user input to execute RV64 instructions.
@@ -211,11 +364,11 @@ bool interpret(char *instr)
 	switch (expressionInt)
 	{
 	case 1:
-		printf("LW \n");
+		lw(tokens);
 		break;
 
 	case 2:
-		printf("SW \n");
+		sw(tokens);
 		break;
 
 	case 3:
@@ -268,9 +421,13 @@ void write_read_demo()
 
 void print_regs()
 {
-	for (int i = 0; i < 32; i++)
+	int col_size = 10;
+	for (int i = 0; i < 8; i++)
 	{
-		printf("Memory X%i: %i\n", i, reg[i]);
+		printf("X%02i:%.*lld", i, col_size, (long long int)reg[i]);
+		printf(" X%02i:%.*lld", i + 8, col_size, (long long int)reg[i + 8]);
+		printf(" X%02i:%.*lld", i + 16, col_size, (long long int)reg[i + 16]);
+		printf(" X%02i:%.*lld\n", i + 24, col_size, (long long int)reg[i + 24]);
 	}
 }
 
@@ -283,10 +440,28 @@ int main()
 	// Do not write any code between init_regs
 	init_regs(); // DO NOT REMOVE THIS LINE
 
+	print_regs();
+
 	// Below is a sample program to a write-read. Overwrite this with your own code.
-	write_read_demo();
-	char s[] = "ADDI X13 X20 130";
-	char *p = s;
-	interpret(p);
+	//write_read_demo();
+
+	printf(" RV32 Interpreter.\nType RV32 instructions. Use upper-case letters and space as a delimiter.\nEnter 'EOF' character to end program\n");
+
+	char *instruction = malloc(1000 * sizeof(char));
+	bool is_null = false;
+	// fgets() returns null if EOF is reached.
+	is_null = fgets(instruction, 1000, stdin) == NULL;
+	while (!is_null)
+	{
+		interpret(instruction);
+		printf("\n");
+		print_regs();
+		printf("\n");
+
+		is_null = fgets(instruction, 1000, stdin) == NULL;
+	}
+
+	printf("Good bye!\n");
+
 	return 0;
 }
